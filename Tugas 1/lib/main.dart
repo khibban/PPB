e1d262f4891da9f503202415a5a0f8fd0e8dba9a
moebiuskh <khibban148@gmail.com>
2026-03-08 11:33:24 +0700

    tugas 1

---
+++ Tugas 1/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('My App'), // Melengkapi bagian yang terpotong
+      ),
+      body: Center(
+        child: Text(
+          'Henshin',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 3,
+            color: Colors.blueAccent,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // child: const Icon(Icons.add),
+        backgroundColor: Colors.amber,
+        onPressed: () {},
+      ),
+    ),
+  ));
+}
